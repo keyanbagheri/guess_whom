e530a5aa2898f9c8f6c7a28575406bd87424f14c
cdbajorin <christopher.bajorin@gmail.com>
2014-05-09 12:34:14 -0400

    Added potential route structure

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,32 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+####### Moved examples to the bottom for reference
+
+####### Potential routes
+
+    # root 'cards#index'
+
+    get 'cards/show' =>
+
+    get 'cards/create' =>
+
+    get 'cards/:id/update' =>
+
+    get 'cards/:id/delete' =>
+
+    # instructions with a "Play Game!" button
+    get 'game/instructions' =>
+
+    # actual game instance
+    get 'game/play' =>
+
+
+end
+
+
+
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
@@ -53,4 +74,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
