95aa114b3ce2adc5f6d250d182e95763d29384ad
cdbajorin <christopher.bajorin@gmail.com>
2014-05-14 17:09:48 -0400

    file name change on temp files

--- app/controllers/temp_api.rb
+++ app/controllers/temp_api.rb
@@ -1,8 +1,22 @@
 ###### Temporary imaginary controller for cards generation
 class CardsController < ActionController::Base
 
-
   def new
+    url = "https://api.instagram.com/v1/tags/selfie/media/recent?client_id=4103708a8bb2413ea434757ae68c811f"
+    response = HTTParty.get(url)
+    full_selfie_array = []
+    @selfie_array = []
+    numbers = (0...20).to_a.sample 9
+    numbers.each do |x|
+      image_url = response["data"][x]["images"]["standard_resolution"]["url"]
+      username = response["data"][x]["user"]["username"]
+      current_selfie_array = [username, image_url]
+      @selfie_array << current_selfie_array
+    end
+  end
+
+
+  def create
     Card.create(card_attributes)
   end
 
