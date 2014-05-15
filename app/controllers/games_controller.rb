08fe128f32166f3c0c3ae185f696b4f9670a0dd9
johntrandall <john@johnrandall.com>
2014-05-14 21:32:31 -0400

    finished game reset, added to routes and controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,6 +3,10 @@ class GamesController < ActionController::Base
 	#### CREATE A NEW GAME RE-WRITE. MOVE GUTS TO MODEL
 	def reset_game
 		@current_game = Game.start_game
+		# render json: @current_game
+
+		redirect_to "/play/#{@current_game.id}"
+		# play()
   	end
 
 
@@ -11,6 +15,12 @@ class GamesController < ActionController::Base
 	
 
 	def play
+		# render json: @current_game.cards
+		@current_game = Game.find(params[:game_id])
+		
+		# render json: @current_game
+		# render json: @current_game.cards
+
 		#play page. reload
 	end
 
